95282b273256a772e273e40176dbe8c871a5c150
Brian High <brianhigh@users.noreply.github.com>
2019-06-06 12:00:17 -0700

    Update get_pollen_history.R

--- get_pollen_history.R
+++ get_pollen_history.R
@@ -2,21 +2,32 @@
 # Author: Brian High
 # Date: 2019-06-06
 
-if (!require(pacman)) install.packages('pacman')
+# Load packages.
+if (!require(pacman)) {
+  install.packages('pacman', repos = 'http://cran.r-project.org')
+}
 pacman::p_load(RCurl, jsonlite)
 
-# Get historic pollen count given a zipcode and number of days.
+# Get historic pollen index given a zipcode and number of days.
 get_pollen <- function(zip = 98104, days = '30') {
+  # Define variables.
   base_url <- 'https://www.pollen.com'
+  forecast_path <- 'forecast/historic/pollen'
+  referer <- paste(base_url, forecast_path, zip, sep = '/')
   useragent <- 'Mozilla/5.0'
-  referer <- paste0(base_url, '/forecast/historic/pollen/', zip)
-  URL <- paste0(base_url, '/api/forecast/historic/pollen/', zip, '/', days)
+    
+  # Get data.
+  URL <- paste(base_url, 'api', forecast_Path, zip, days, sep = '/')
   json_data <- getURL(URL, referer = referer, useragent = useragent)
   pollen <- fromJSON(json_data)$Location$periods
+  
+  # Clean data.
   pollen$Period <- as.Date(gsub('T.*$', '', pollen$Period))
   pollen$zip <- zip
   names(pollen) <- c('date', 'pollen_index', 'zip')
-  pollen
+  
+  return(pollen)
 }
 
+# Get historical pollen index for a specific zip code and number of days.
 get_pollen(zip = '98105', days = '60')
